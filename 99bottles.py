ba7486cba70782842dfc4f815d7127c7a6990e3e
vinchang920 <35587977+vinchang920@users.noreply.github.com>
2018-01-18 21:02:09 -0700

    Create 99bottles.py

---
+++ 99bottles.py
@@ -0,0 +1,6 @@
+x=99
+while (x>1):
+    print ( str(x) + " bottles of beer on the wall, " + str(x) + " bottles of beer! Take one down, pass it around, " + str(x) + " bottles of beer on the wall!")
+    x-=1
+else:
+    print  (str(x) + " bottle of beer on the wall, " + str(x) + " bottle of beer! Take it down, pass it around, " + str(x) + " bottle of beer on the wall!")

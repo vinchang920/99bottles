d20badd998948d81b91d77c7b7a8228e9e75cb78
vinchang920 <35587977+vinchang920@users.noreply.github.com>
2018-01-22 09:25:25 -0700

    Updated app

--- VC99bottles.py
+++ VC99bottles.py
@@ -1,6 +1,7 @@
-x=99
-while (x>1):
-    print ( str(x) + " bottles of beer on the wall, " + str(x) + " bottles of beer! Take one down, pass it around, " + str(x) + " bottles of beer on the wall!")
-    x-=1
+a=99
+while (a>1):
+    print a, "bottles of rum on the wall,", a, "bottles of rum, take one down, pass it around,"
+    a -= 1
+    print a, "bottles of rum on the wall!"
 else:
-    print  (str(x) + " bottle of beer on the wall, " + str(x) + " bottle of beer! Take it down, pass it around, " + str(x) + " bottle of beer on the wall!")
+    print a, "bottle of rum on the wall!", a, "bottle of rum! Take one down, pass it around,", a, "bottle of rum on the wall!" 
